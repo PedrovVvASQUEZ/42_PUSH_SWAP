372071b7d3efc8f1d0cae9ff680380169e516a49
Pierre Grellier-beloqui <pgrellie@bess-f3r6s4.clusters.42paris.fr>
2024-05-17 18:49:56 +0200

    push_swap update

--- push_swap_bonus/parsing_bonus.c
+++ push_swap_bonus/parsing_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing_bonus.c                                    :+:      :+:    :+:   */
+/*   parsing_bonus.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgrellie <pgrellie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 15:36:27 by pgrellie          #+#    #+#             */
-/*   Updated: 2024/04/23 18:51:07 by pgrellie         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:55:16 by pgrellie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#include "push_swap.h"
 
 static int	check_fs(char *s)
 {
@@ -23,6 +23,8 @@ static int	check_fs(char *s)
 			return (0);
 		else if ((ft_issigns(s[x]) && ft_issigns(s[x + 1]))
 			|| (ft_issigns(s[x]) && ft_isspace(s[x + 1])))
+			return (0);
+		else if (!ft_isdigit(s[x]) && s[x + 1] == '\0')
 			return (0);
 		x++;
 	}
@@ -40,7 +42,7 @@ static char	*concat(int ac, char **av)
 		y = 1;
 		dest = malloc(sizeof(char *) * y + 1);
 		if (!dest)
-			return (NULL);
+			return (free(dest), NULL);
 		tmp = dest;
 		dest = ft_strjoin(av[y], av[y + 1]);
 		free(tmp);
@@ -56,7 +58,7 @@ static char	*concat(int ac, char **av)
 	return (av[1]);
 }
 
-bool	only_space_bo(char *str)
+bool	only_space(char *str)
 {
 	int	i;
 
@@ -70,7 +72,14 @@ bool	only_space_bo(char *str)
 	return (true);
 }
 
-char	**final_flash_bo(int ac, char **av)
+static void	free_fs(char *fs, int ac)
+{
+	write(STDERR_FILENO, "Error\n", 6);
+	if (ac > 2)
+		free(fs);
+}
+
+char	**final_flash(int ac, char **av)
 {
 	char	*fs;
 	char	**tab;
@@ -78,21 +87,21 @@ char	**final_flash_bo(int ac, char **av)
 	tab = NULL;
 	fs = concat(ac, av);
 	if (check_fs(fs) == 0)
-	{
-		write(1, "Error\n", 6);
-		return (free(fs), NULL);
-	}
-	else if (only_space_bo(fs))
-	{
-		write(1, "Error\n", 6);
-		return (free(fs), NULL);
-	}
+		return (free_fs(fs, ac), NULL);
+	else if (only_space(fs))
+		return (free_fs(fs, ac), NULL);
 	else if (ac == 2 && check_fs(av[1]) == 1)
+	{
 		tab = ft_split(av[1], ' ');
+		if (!tab)
+			return (NULL);
+	}
 	else if (ac > 2 && check_fs(fs) == 1)
 	{
 		tab = ft_split(fs, ' ');
 		free(fs);
+		if (!tab)
+			return (NULL);
 	}
 	else
 		return (NULL);
